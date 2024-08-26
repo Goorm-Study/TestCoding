613be6bf42a0d000b65add5089905ea05e778389
YuyoungRhee <bbclara@naver.com>
2024-08-27 00:45:14 +0900

    section3 실습

---
+++ src/main/java/sample/cafekiosk/unit/order/Order.java
@@ -0,0 +1,18 @@
+package sample.cafekiosk.unit.order;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import sample.cafekiosk.unit.beverage.Beverage;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public class Order {
+
+    private final LocalDateTime orderDateTime;
+    private final List<Beverage> beverages;
+
+}
