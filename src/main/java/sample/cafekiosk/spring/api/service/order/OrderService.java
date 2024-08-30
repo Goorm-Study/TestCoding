bc7236015dc0aa02aa9ce0dd0163677ad1ad3db3
wjd4204 <brian876373@gmail.com>
2024-08-30 23:42:44 +0900

    feat: 중복되는 상품에 대한 주문 전송

---
+++ src/main/java/sample/cafekiosk/spring/api/service/order/OrderService.java
@@ -0,0 +1,45 @@
+package sample.cafekiosk.spring.api.service.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.service.order.request.OrderCreateRequest;
+import sample.cafekiosk.spring.api.service.order.response.OrderResponse;
+import sample.cafekiosk.spring.domain.order.Order;
+import sample.cafekiosk.spring.domain.order.OrderRepository;
+import sample.cafekiosk.spring.domain.product.Product;
+import sample.cafekiosk.spring.domain.product.ProductRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
+@RequiredArgsConstructor
+@Service
+public class OrderService {
+
+    private final ProductRepository productRepository;
+    private final OrderRepository orderRepository;
+
+    public OrderResponse createOrder(OrderCreateRequest request, LocalDateTime registeredDate){
+        List<String> productNumbers = request.getProductNumbers();
+        List<Product> products = findProductsBy(productNumbers);
+
+        Order order = Order.create(products, registeredDate);
+        Order savedOrder = orderRepository.save(order);
+
+        return OrderResponse.of(savedOrder);
+    }
+
+    private List<Product> findProductsBy(List<String> productNumbers) {
+        List<Product> products = productRepository.findAllByProductNumberIn(productNumbers);
+        Map<String, Product> productMap = products.stream()
+                .collect(Collectors.toMap(product -> product.getProductNumber(), p -> p));
+
+        return productNumbers.stream()
+                .map(productNumber -> productMap.get(productNumber))
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/sample/cafekiosk/spring/domain/order/Order.java
@@ -0,0 +1,52 @@
+package sample.cafekiosk.spring.domain.order;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import sample.cafekiosk.spring.domain.BaseTimeEntity;
+import sample.cafekiosk.spring.domain.orderProduct.OrderProduct;
+import sample.cafekiosk.spring.domain.product.Product;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "orders")
+@Entity
+public class Order extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    private int totalPrice;
+
+    private LocalDateTime registeredDateTime;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderProduct> orderProducts = new ArrayList<>();
+
+    public Order(List<Product> products, LocalDateTime registeredDateTime){
+        this.orderStatus = OrderStatus.INIT; // order의 초기값이나 등록 시간을 부여해야 하는 test 작성!
+        this.totalPrice = calculateTotalPrice(products);
+        this.registeredDateTime = registeredDateTime;
+        this.orderProducts = products.stream()
+                .map(product -> new OrderProduct(this, product))
+                .collect(Collectors.toList());
+    }
+
+    private static int calculateTotalPrice(List<Product> products) {
+        return products.stream().mapToInt(Product::getPrice).sum();
+    }
+
+    public static Order create(List<Product> products, LocalDateTime registeredDateTime) {
+        return new Order(products, registeredDateTime);
+    }
+}
